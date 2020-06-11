96d846242e602ee71148f0f4aa7ed2aca0456722
kumud garg <kumud.garg@kumuds-MacBook-Pro.local>
2020-06-11 11:16:11 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Navabr = ({ totalCounters }) => {
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				Navbar <span className="badge badge-pill badge-secondary">{totalCounters}</span>
+			</a>
+		</nav>
+	);
+};
+
+export default Navabr;
